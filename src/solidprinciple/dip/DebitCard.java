4b48148b01f0ded1a0fb95f966db7dd4c3285e83
unknown <yunikashakya082@gmail.com>
2022-12-15 10:44:01 -0800

    changes

--- src/solidprinciple/dip/DebitCard.java
+++ src/solidprinciple/dip/DebitCard.java
@@ -3,6 +3,6 @@ package solidprinciple.dip;
 public class DebitCard implements BankCard {
 
     public void doTransaction(long amt){
-        System.out.println("This is pay,ment with debit card");
+        System.out.println("This is payment with debit card");
     }
 }
--- src/solidprinciple/dip/MainClass.java
+++ src/solidprinciple/dip/MainClass.java
@@ -3,7 +3,7 @@ package solidprinciple.dip;
 public class MainClass {
 
     public static void main(String[] args) {
-        BankCard bankCard = new DebitCard();
+        BankCard bankCard = new CreditCard();
         ShoppingMall shoppingMall = new ShoppingMall(bankCard);
         shoppingMall.purchase(5000);
     }
